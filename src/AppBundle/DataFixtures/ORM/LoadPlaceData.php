2a4583baa826c16ef67287f1a6e20bc27242ca5a
Guillaume SARRAMEGNA <guillaume.sarramegna@acelys.fr>
2017-04-25 11:15:59 +0200

    Update place fixture with price

--- src/AppBundle/DataFixtures/ORM/LoadPlaceData.php
+++ src/AppBundle/DataFixtures/ORM/LoadPlaceData.php
@@ -3,6 +3,7 @@
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\Place;
+use AppBundle\Entity\Price;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -13,12 +14,18 @@ class LoadPlaceData implements FixtureInterface
         $places = [[
             'name' => 'Tour Eiffel',
             'address' => '5 Avenue Anatole France, 75007 Paris',
+            'prices' => [[
+                'type' => 'less_than_12',
+                'value' => 5.75,
+            ]],
         ], [
             'name' => 'Mont-Saint-Michel',
             'address' => '50170 Le Mont-Saint-Michel',
+            'prices' => [],
         ], [
             'name' => 'Château de Versailles',
             'address' => 'Place d\'Armes, 78000 Versailles',
+            'prices' => [],
         ]];
 
         foreach ($places as $p) {
@@ -28,6 +35,16 @@ class LoadPlaceData implements FixtureInterface
                 ->setAddress($p['address'])
             ;
             $manager->persist($place);
+
+            foreach ($p['prices'] as $pr) {
+                $price = new Price();
+                $price
+                    ->setPlace($place)
+                    ->setType($pr['type'])
+                    ->setValue($pr['value'])
+                ;
+                $manager->persist($price);
+            }
         }
 
         $manager->flush();
